0328dbece38d813c13b5dbb90084656bc9058c51
Oh JiSun <ohjeesun@naver.com>
2023-08-11 01:00:23 +0900

    Update : api url

--- web/src/App.js
+++ web/src/App.js
@@ -6,7 +6,7 @@ function App() {
    const [hello, setHello] = useState('')
 
     useEffect(() => {
-        axios.get('/api/hello')
+        axios.get('/success')
         .then(response => setHello(response.data))
         .catch(error => console.log(error))
     }, []);
