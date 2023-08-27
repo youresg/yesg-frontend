24f316fc29d8df72d97453b60975c92d7967ca54
Oh JiSun <ohjeesun@naver.com>
2023-08-27 15:53:33 +0900

    Feat : Login

--- web/src/components/main.js
+++ web/src/components/main.js
@@ -3,9 +3,10 @@ import React from 'react';
 const main=() =>{
     return(
         <div className="mainPage">
+        <div><a href="http://localhost:8080/oauth2/authorization/google">구글 계정</a></div>
         <div className="image-container"><img className="ment" alt="ment" src="img/ment2.png" /></div>
         <div className="slide-container">
-      <img className="emoji" alt="emoji" src="img/emoji.png" /> </div>
+        <img className="emoji" alt="emoji" src="img/emoji.png" /> </div>
         <div><img className="back" alt="back" src="img/mainColor1.png" /></div>
         <div><img className="earth" alt="earth" src="img/earth3.png" /></div>
     </div>
